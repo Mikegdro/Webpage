60157bd09dec1544cdc933cac80d07dd71b48dae
Miguel García del Real Ortiz <miguelgdro@gmail.com>
2025-05-21 16:16:58 +0200

    New component, re-structuring of the app

---
+++ app/components/content/PageSection.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <UContainer>
+        <h2>
+            <slot name="title" />
+        </h2>
+        <p>
+            <slot name="description" />
+        </p>
+        <slot />
+    </UContainer>
+</template>
---
+++ app/components/content/RecentPosts.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <UContainer>
+        Posts
+    </UContainer>
+</template>
